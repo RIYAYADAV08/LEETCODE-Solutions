c8a73abd3eceb83a255e793b1fe83955ffb0ee5a
RIYA YADAV <riya080305@gmail.com>
2026-07-20 08:25:45 +0530

    Time: 9 ms (99.72%), Space: 86.6 MB (94.55%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+
+        int n = nums.length;
+
+        if (n < 2)
+            return 0;
+
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        if (min == max)
+            return 0;
+
+        int bucketSize = Math.max(1, (int) Math.ceil((double) (max - min) / (n - 1)));
+        int bucketCount = (max - min) / bucketSize + 1;
+
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+        boolean[] used = new boolean[bucketCount];
+
+        for (int i = 0; i < bucketCount; i++) {
+            bucketMin[i] = Integer.MAX_VALUE;
+            bucketMax[i] = Integer.MIN_VALUE;
+        }
+
+        for (int num : nums) {
+            int idx = (num - min) / bucketSize;
+
+            bucketMin[idx] = Math.min(bucketMin[idx], num);
+            bucketMax[idx] = Math.max(bucketMax[idx], num);
+            used[idx] = true;
+        }
+
+        int ans = 0;
+        int prev = min;
+
+        for (int i = 0; i < bucketCount; i++) {
+
+            if (!used[i])
+                continue;
+
+            ans = Math.max(ans, bucketMin[i] - prev);
+
+            prev = bucketMax[i];
+        }
+
+        return ans;
+    }
+}
