1274b3edcf4aec8ca5dbb96355961f52a204f25f
RIYA YADAV <riya080305@gmail.com>
2026-07-27 21:30:57 +0530

    Time: 18 ms (86.78%), Space: 196.7 MB (16.38%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+
+    private Map<Integer, Integer> map = new HashMap<>();
+    private Boolean[][] dp;
+
+    public boolean canCross(int[] stones) {
+
+        int n = stones.length;
+
+        for (int i = 0; i < n; i++) {
+            map.put(stones[i], i);
+        }
+
+        dp = new Boolean[n][n + 1];
+
+        return dfs(stones, 0, 0);
+    }
+
+    private boolean dfs(int[] stones, int index, int jump) {
+
+        if (index == stones.length - 1) {
+            return true;
+        }
+
+        if (dp[index][jump] != null) {
+            return dp[index][jump];
+        }
+
+        for (int nextJump = jump - 1; nextJump <= jump + 1; nextJump++) {
+
+            if (nextJump <= 0) continue;
+
+            int nextStone = stones[index] + nextJump;
+
+            if (map.containsKey(nextStone)) {
+
+                if (dfs(stones, map.get(nextStone), nextJump)) {
+                    return dp[index][jump] = true;
+                }
+            }
+        }
+
+        return dp[index][jump] = false;
+    }
+}
